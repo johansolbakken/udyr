49a603c484c61fdffe429129da1b995b393fd7f2
Johan Solbakken <johsol@stud.ntnu.no>
2023-02-12 15:10:19 +0100

    Add basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
-mod app;
+use std::env;
+use std::io;
+use std::io::Write;
+
+fn run(line: &String) {
+    println!("{}", line)
+}
+
+fn run_file(path: &String) {
+    let source = std::fs::read_to_string(path).unwrap();
+    run(&source);
+}
+
+fn run_prompt() {
+    let mut line = String::new();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let bytes_read = std::io::stdin().read_line(&mut line).unwrap();
+        if bytes_read == 1 && line == "\n" {
+            break;
+        }
+        run(&line);
+    }
+}
 
 fn main() {
-    let mut app = app::application::Application::new();
-    app.run();
-    app.destroy();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: udyr [script]")
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
 }
